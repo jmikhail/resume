34934923e75a5bd7d561a11243b1c39587595bd5
janmikhail <jpmikhail@gmail.com>
2019-09-23 18:30:03 -0400

    Updates

---
+++ precache-manifest.a0ffa9a88c9d4d4455e3ab870a244c9c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bca65bf0470fc801d24b4b3d0a7147",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "7f6513b605b7c8a270a6",
+    "url": "/resume/static/css/main.d06d7907.chunk.css"
+  },
+  {
+    "revision": "b9f313f7a941136ed979",
+    "url": "/resume/static/js/2.49e698ae.chunk.js"
+  },
+  {
+    "revision": "7f6513b605b7c8a270a6",
+    "url": "/resume/static/js/main.b98024d4.chunk.js"
+  },
+  {
+    "revision": "17cc7308dea17913c7b4",
+    "url": "/resume/static/js/runtime~main.deee99b2.js"
+  },
+  {
+    "revision": "7ef42a2e86274e7508cf6c4cb3f882f7",
+    "url": "/resume/static/media/buzzyn_businesshub.7ef42a2e.jpg"
+  },
+  {
+    "revision": "b463347a6ada30894141aee840841476",
+    "url": "/resume/static/media/buzzyn_pos.b463347a.jpg"
+  },
+  {
+    "revision": "94a98c14bec8eae6811e61ed331ebffb",
+    "url": "/resume/static/media/buzzyn_rewards.94a98c14.jpg"
+  },
+  {
+    "revision": "8ff9e5e0f8710ce541af520ebab1195e",
+    "url": "/resume/static/media/logo_alcatellucent.8ff9e5e0.jpg"
+  },
+  {
+    "revision": "27736129f73a75ff9231ff0ef21ae3e6",
+    "url": "/resume/static/media/logo_blackberry.27736129.jpg"
+  },
+  {
+    "revision": "19471a4c0e89e5bd793e35322db01c48",
+    "url": "/resume/static/media/logo_buzzyn.19471a4c.jpg"
+  },
+  {
+    "revision": "b5a8c3f95f03949508e9953b28f67815",
+    "url": "/resume/static/media/logo_carleton.b5a8c3f9.jpg"
+  },
+  {
+    "revision": "dc53e6a6f985cacb1523dbb68bbb6ce4",
+    "url": "/resume/static/media/me.dc53e6a6.jpg"
+  }
+]);
